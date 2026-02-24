b0377bf9f310ca8e152450ec72a3a166ffe23f5b
CuzMi <simon.weijie@gmail.com>
2026-02-24 11:23:44 +0800

    Change BPETokenizer.py

--- cs336_basics/BPETokenizer.py
+++ cs336_basics/BPETokenizer.py
@@ -6,6 +6,7 @@ c. Train on TinyStories and OpenWebText
 d. Leaderboard: minimize OpenWebText perplexity given 90 minutes on a H100
 """
 import heapq
+from collections import defaultdict
 
 
 class BPETokenizer():
@@ -75,99 +76,51 @@ class BPETokenizer2():
         self.vocab_list = []
         self.merge_rule = []
 
-    def train(self, text, num_merges = 100): # English text
-        # 正则匹配获得一些word chunk
-        chunk_list = list(set(self.pretokenizer(text)))
+    def train(self, text, chunk_dict,num_merges = 100): # English text
+        # chunk _dict = {chunk1: freq, chunk2: freq,...}
+        # init part
+        pair_freq = defaultdict(int) # pair = tuple (a,b)
+        pair_chunk = defaultdict(set)
+        chunk_pair = defaultdict(list)
+        heap = []
+        for chunk, freq in chunk_dict.items():
+            chunk_list = list(chunk)
+            for idx in range(len(chunk_list) - 1):
+                # 分解chunk得到pair_freq
+                pair = (chunk_list[idx], chunk_list[idx + 1])
+                pair_freq[pair] += freq
+                # 维护pair to chunk
+                pair_chunk[pair].add(chunk) # 这里collection.defaultdict 的用法/ 以及add和append<返回None>    PPPPPPPPPPPPoint1
+                chunk_pair[chunk].add(pair)
+       
+        
+        # 维护最大堆 pair_freq -> heap
+        for pair, freq in pair_freq.items():
+           heapq.heappush(heap, (-freq, pair))  # 维护堆                        PPPPPPPPPPPPoint2
+        
+        # loop part
+        while num_merges > 0 or heap[0][0] != -1:
+            max_freq, max_pair = heap[0]
+            max_freq = - max_freq
+            self.merge_rule.append(max_pair)
 
-        # 正向匹配 - 频率
-        chunk_freq = {'<unk>': 0}
-        current_idx = 0
-
-        while current_idx < len(text):
-            first_letter = text[current_idx]
-
-            candidates = [chunk for chunk in chunk_list if chunk.startswith(first_letter)]
-            candidates.sort(key=len, reverse=True)
-
-            match = False
-            for candidate in candidates:
-                if text.startswith(candidate, current_idx):
-                    match = True
-                    current_idx += len(candidate)
-
-                    # chunk内部划分
-                    freq_name = " ".join(candidate)
-
-                    chunk_freq[freq_name] = chunk_freq.get(freq_name, 0) + 1
-
-                    break
+            # 定位pair 影响的pair
+            chunks = pair_chunk[max_pair] # [chunk1, chunk2, ...]
+            for chunk in chunks:          
                 
-            if not match:
-                chunk_freq['<unk>'] += 1
-                current_idx += 1
-            
-        # 生成对应的pair"_freq & 反向索引
-        pair_freq = {}
-        pair2chunk = {}
-        for name, freq in chunk_freq.items():
-            if name == '<unk>':
-                continue
-            for idx in range(len(name)-1):
-                pair = (name[idx], name[idx+1]) # ('l','o')
 
-                pair_freq[pair] = pair_freq.get(pair, 0) + freq
-                pair2chunk[pair] = pair2chunk.get(pair, []).append(name)
+        
+        
+               
 
-        for _ in range(num_merges):      
-            # 记录内部符号的全局频率 - 最大堆
-            max_heap = []
-            for pair, freq in pair_freq.items():
-                if name == '<unk>':
-                    continue
-                heapq.heappush(max_heap, (-freq, pair))
-            
-            # 取出最高频率词，记录合并规则
-            max_pair = self.max_heap[0][1]
-            max_freq = - self.max_heap[0][0]
-            self.merge_rule.append([max_pair])
-
-            # 定位受影响词，然后定位受影响pair，改名
-            influ_chunks = pair2chunk[max_pair]
-            for chunk in influ_chunks:
-                for idx in range(len(chunk)-1):
-                
-                    if chunk[idx] == max_pair[0] and chunk.startswith(max_pair, idx):
-                        # 如果是开头
-                        if idx == 0 and "".join(max_pair) == chunk:
-                            past_chunk = None
-                            next_chunk = None
-                        if idx == 0:
-                            next_chunk = (chunk[idx+1], chunk[idx+2])
-                        
-                        if idx + 1 == len(chunk):
-                            next_chunk = None
-                        else:
-                            next_chunk = (chunk[idx+1], chunk[idx+2])
-                
-                if past_chunk:
-                    new_chunk = (past_chunk[0], "".join(max_pair))
-                    pair_freq[new_chunk] = pair_freq.pop(past_chunk)
-                    pair2chunk[new_chunk] = pair2chunk.pop(past_chunk)
-                
-                if next_chunk:
-                    new_chunk = (next_chunk[0], "".join(max_pair))
-                    pair_freq[new_chunk] = pair_freq.pop(next_chunk)
-                    pair2chunk[new_chunk] = pair2chunk.pop(next_chunk)
-
-                if past_chunk or next_chunk:
-                    _ = pair_freq.pop(max_pair)
-                    _ = pair2chunk.pop(max_pair)
+           
 
             
         
+    def pretokenizer(self) -> dict: 
+        chunk_dict ={}
 
-    
-    def pretokenizer(self):
+        return chunk_dict
 
 
 
